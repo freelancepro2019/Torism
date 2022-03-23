139bf8bd90b87417318b3e7b4c74aa4f7d437fec
Emad <emadmagdy.developer@gmail.com>
2022-03-23 23:03:54 +0200

    emad

--- app/src/main/java/com/app/tourism/uis/activity_home/fragments/home_module/FragmentHome.java
+++ app/src/main/java/com/app/tourism/uis/activity_home/fragments/home_module/FragmentHome.java
@@ -18,12 +18,14 @@ import android.widget.Toast;
 
 import com.app.tourism.R;
 import com.app.tourism.adapters.GuideAdapter;
+import com.app.tourism.databinding.FilterDialogBinding;
 import com.app.tourism.databinding.FragmentHomeBinding;
 import com.app.tourism.models.UserModel;
 import com.app.tourism.tags.Tags;
 import com.app.tourism.uis.activity_send_order.SendOrderActivity;
 import com.app.tourism.uis.common_ui.activity_base.FragmentBase;
 import com.app.tourism.uis.activity_home.HomeActivity;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -41,6 +43,8 @@ public class FragmentHome extends FragmentBase {
     private FragmentHomeBinding binding;
     private GuideAdapter adapter;
     private DatabaseReference dRef;
+    private String filterBy ="all";
+    private String dialogFilterQuery = filterBy;
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -63,19 +67,23 @@ public class FragmentHome extends FragmentBase {
 
     private void initView() {
         dRef = FirebaseDatabase.getInstance().getReference();
+        binding.setFilter(getString(R.string.All));
         binding.recViewLayout.recView.setLayoutManager(new LinearLayoutManager(activity));
         adapter = new GuideAdapter(activity,getLang(),this);
         binding.recViewLayout.recView.setAdapter(adapter);
         binding.recViewLayout.swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
-        binding.recViewLayout.swipeRefresh.setOnRefreshListener(this::getGuides);
+        binding.recViewLayout.swipeRefresh.setOnRefreshListener(()->getGuides(filterBy));
         binding.recViewLayout.tvNoData.setText(R.string.no_guide);
         binding.cardViewSearch.setOnClickListener(view -> {
             Navigation.findNavController(view).navigate(R.id.searchFragment);
         });
-        getGuides();
+        binding.filter.setOnClickListener(view -> showFilterDialog());
+        binding.tvFilter.setOnClickListener(view -> showFilterDialog());
+
+        getGuides(filterBy);
     }
 
-    private void getGuides() {
+    private void getGuides(String filterBy) {
         binding.recViewLayout.swipeRefresh.setRefreshing(true);
         binding.recViewLayout.tvNoData.setVisibility(View.GONE);
         Query query = dRef.child(Tags.USERS_TABLE);
@@ -91,7 +99,13 @@ public class FragmentHome extends FragmentBase {
                             for (DataSnapshot ds :snapshot.getChildren()){
                                 UserModel userModel = ds.getValue(UserModel.class);
                                 if (userModel!=null){
-                                    guidesList.add(userModel);
+                                    if (filterBy.equals("all")){
+                                        guidesList.add(userModel);
+
+                                    }else if (filterBy.equals(userModel.getGender())){
+                                        guidesList.add(userModel);
+
+                                    }
                                 }
                             }
 
@@ -99,6 +113,8 @@ public class FragmentHome extends FragmentBase {
                                 adapter.updateList(sortedList(guidesList));
                                 binding.recViewLayout.tvNoData.setVisibility(View.GONE);
                             }else {
+                                adapter.updateList(new ArrayList<>());
+
                                 binding.recViewLayout.tvNoData.setVisibility(View.VISIBLE);
 
                             }
@@ -115,6 +131,63 @@ public class FragmentHome extends FragmentBase {
     private List<UserModel> sortedList(List<UserModel> list) {
         Collections.sort(list, (m1, m2) -> Double.compare(m2.getRate(),m1.getRate()));
         return list;
+    }
+
+    private void showFilterDialog(){
+        BottomSheetDialog dialog = new BottomSheetDialog(activity);
+        dialog.setCancelable(true);
+        dialog.setCanceledOnTouchOutside(true);
+        FilterDialogBinding filterDialogBinding = DataBindingUtil.inflate(LayoutInflater.from(activity),R.layout.filter_dialog,null,false);
+        if (filterBy.equals(Tags.male)){
+            filterDialogBinding.rbMale.setChecked(true);
+        }else if (filterBy.equals(Tags.female)){
+            filterDialogBinding.rbFemale.setChecked(true);
+
+        }else {
+            filterDialogBinding.rbAll.setChecked(true);
+
+        }
+
+        filterDialogBinding.rbAll.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b){
+                dialogFilterQuery ="all";
+            }
+        });
+        filterDialogBinding.rbMale.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b){
+                dialogFilterQuery = Tags.male;
+            }
+        });
+
+        filterDialogBinding.rbFemale.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b){
+                dialogFilterQuery = Tags.female;
+            }
+        });
+
+        filterDialogBinding.btnFilter.setOnClickListener(view -> {
+            filterBy = dialogFilterQuery;
+
+
+            if (dialogFilterQuery.equals("all")){
+                binding.setFilter(getString(R.string.All));
+            }else if (dialogFilterQuery.equals(Tags.male)){
+                binding.setFilter(getString(R.string.male));
+            }else if (dialogFilterQuery.equals(Tags.female)){
+                binding.setFilter(getString(R.string.female));
+
+            }
+
+            getGuides(filterBy);
+
+
+            dialog.dismiss();
+        });
+
+        filterDialogBinding.imageClose.setOnClickListener(view -> dialog.dismiss());
+
+        dialog.setContentView(filterDialogBinding.getRoot());
+        dialog.show();
     }
 
     public void setItemData(UserModel guideModel) {
