a44d24eadfcdd1b3f0c7a6ad8eba95a25d57dd7e
El3shry <ayanada816@gmail.com>
2022-05-14 02:25:49 +0200

    el34ry

--- app/src/main/java/com/app/tourism/uis/activity_home/fragments/camera_module/FragmentCamera.java
+++ app/src/main/java/com/app/tourism/uis/activity_home/fragments/camera_module/FragmentCamera.java
@@ -206,7 +206,7 @@ public class FragmentCamera extends FragmentBase {
     private void getSearchResults(String searchQuery) {
         String apikey = "89bb969b41006602b5b3f1fd14f2f2c7192afbc3928038297eb15c297ea1cd54";
         Log.e("xxxxx",searchQuery.trim() );
-        String url = "https://serpapi.com/search.json?q=" + searchQuery.trim() + "&location=Saudi Arabia&hl=ar&gl=us&google_domain=google.com&api_key=" + apikey;
+        String url = "https://serpapi.com/search.json?q=" + searchQuery.trim() + "&location=Saudi Arabia&hl=ar&gl=sa&google_domain=google.com&api_key=" + apikey;
         RequestQueue queue = Volley.newRequestQueue(activity);
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
